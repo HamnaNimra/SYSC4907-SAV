ec5df6e6cab75f7335e9e5a7b7221cba98087e4c
Frank Yu <frankyu1999@gmail.com>
2021-11-06 23:18:38 -0400

    Stub mapping and routing

--- src/mapping_navigation/scripts/map_information.py
+++ src/mapping_navigation/scripts/map_information.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python3
 from enum import Enum
+from typing import Dict, Tuple, List
 
-class Map:
-
-    def __init__(self, roads):
-        self.roads = {}
-
-class Point:
-
-    def __init__(self, coordinate, intersection_ID = None):
-        self.coordinate = coordinate
-        self.intersection_ID = intersection_ID
-
-class RoadSegment:
-
-    def __init__(self, lanes , road_segment_ID: int):
-        self.lanes = lanes
-        self.road_segment_ID = road_segment_ID
-
-class Lane:
-
-    def __init__(self, points):
-        self.points = points
 
 class DirEnum(Enum):
     NORTH = 1
     SOUTH = 2
     EAST = 3
     WEST = 4
+
+
+class Point:
+    def __init__(self, coordinate: Tuple[int, int], intersection_id: int = None):
+        self.coordinate = coordinate
+        self.intersection_id = intersection_id
+
+
+class Lane:
+    def __init__(self, points: List[Point]):
+        self.points = points
+
+
+class RoadSegment:
+    def __init__(self, lanes: List[Lane], road_segment_id: int):
+        self.lanes = lanes
+        self.road_segment_id = road_segment_id
+
+
+class Map:
+    def __init__(self, roads: Dict[int, Dict[DirEnum, RoadSegment]] = {}):
+        self.roads = roads
