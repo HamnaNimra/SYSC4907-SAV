dad3f24761d75d6b00fd48cd6476685bb60f1205
AndrewHall797 <andrew.hall286@gmail.com>
2021-11-29 21:19:59 -0500

    Integrated into ROS

--- src/mapping_navigation/scripts/navigation.py
+++ src/mapping_navigation/scripts/navigation.py
@@ -2,33 +2,52 @@
 
 import rospy
 from std_msgs.msg import Float64, String
-from mapping_navigation.msg import PathData
 from geometry_msgs.msg import PoseStamped
+from pure_pursuit import *
+from std_msgs.msg import Float64
+from simple_path import *
 
 
 class Navigation:
 
     """
-    This does mapping
+    This does navigation
     """
 
-    def __init__(self):
+    def __init__(self, path: List[Point], look_ahead_distance: float, look_forward_gain: float):
         self.steering_pub = rospy.Publisher('steering', Float64, queue_size=10)
-        self.curve_pub = rospy.Publisher('curve_data', String, queue_size=10)
-        self.path_data = rospy.Publisher('pathData', PathData, queue_size=10)
+        self.path = path
+        self.target_ind = 0
+        self.navigator = PurePursuit(look_ahead_distance, look_forward_gain, path)
+        self.state = State()
 
     def listener(self):
         rospy.init_node('talker', anonymous=True)
         rospy.Subscriber('airsimPose', PoseStamped, self.handle_gps_data)
-        rospy.spin()
+        rospy.Subscriber("sensor/speed", Float64, self.handle_speed_data)        
+        rate = rospy.Rate(30)
 
-    def handle_gps_data(self, data):
-        self.steering_pub.publish(1)
-        self.curve_pub.publish('curve_data')
-        self.path_data.publish(PathData())
-        print('Handles gps data')
+        while not rospy.is_shutdown():
+            self.target_ind, _ = pure_pursuit.search_target_index(state)
+            if len(path)-1 > self.target_ind:
+                steering_angle = self.get_steering_angle()
+                self.steering_pub(steering_pub)
 
+    def handle_gps_data(self, postition: PoseStamped):
+        curr_point = Point((curr_point.pose.position.x, curr_point.pose.position.y))
+        quaterion = (curr_point.pose.orientation.x, curr_point.pose.orientation.y,
+                     curr_point.pose.orientation.z, curr_point.pose.orientation.w)
+        self.state.update_pos(curr_point, quaterion)
+
+    def handle_speed_data(self, speed: Float64):
+        self.state.update_speed(speed.data)
+
+    def get_steering_angle(self) -> float:
+        ackerman_angle_rad, target_ind = navigator.pure_pursuit_steer_control(state)
+        ackerman_angle_def = math.degrees(ackerman_angle_rad) / 45
 
 if __name__ == "__main__":
-    mapping = Navigation()
-    mapping.listener()
+    #Current route stub
+    path = read_points('src\mapping_navigation\scripts\coords.txt')
+    navigation = Navigation(look_ahead_distance=4.0, look_forward_gain=0.1, path=path)
+    navigation.listener()
--- src/mapping_navigation/scripts/pure_pursuit.py
+++ src/mapping_navigation/scripts/pure_pursuit.py
@@ -18,27 +18,30 @@ class State:
     The yaw of the car
     The cars wheel base
     """
-    def __init__(self, wheel_base: float, x: float=0.0, y:float=0.0, yaw:float=0.0, velocity:float=0.0):
+    def __init__(self, wheel_base: float=2.2, point: Point=Point((0,0)), yaw:float=0.0, velocity:float=0.0):
         self.wheel_base = wheel_base
-        self.x = x
-        self.y = y
+        self.point = point
         self.yaw = yaw
         self.velocity = velocity
-        self.rear_x = self.x - ((wheel_base / 2) * math.cos(self.yaw))
-        self.rear_y = self.y - ((wheel_base / 2) * math.sin(self.yaw))
+        self.rear_x = self.point.coordinate[0] - ((wheel_base / 2) * math.cos(self.yaw))
+        self.rear_y = self.point.coordinate[1] - ((wheel_base / 2) * math.sin(self.yaw))
     
     """
     Updates the cars position
     Cartesian coordinates for the cars position
     Quaterion to find the yaw of the car
     """
-    def update(self, x: float, y: float, quaterion: Tuple[float, float, float, float], velocity:float):
-        self.x = x
-        self.y = y
-        self.velocity = velocity
+    def update_pos(self, point: Point, quaterion: Tuple[float, float, float, float]):
+        self.point = point
         self.yaw = euler_from_quaternion(quaterion)
-        self.rear_x = self.x - ((self.wheel_base / 2) * math.cos(self.yaw))
-        self.rear_y = self.y - ((self.wheel_base / 2) * math.sin(self.yaw))
+        self.rear_x = self.point.coordinate[0] - ((self.wheel_base / 2) * math.cos(self.yaw))
+        self.rear_y = self.point.coordinate[1] - ((self.wheel_base / 2) * math.sin(self.yaw))
+
+    """
+    Updates the cars speed
+    """
+    def update_speed(self, velocity:float):
+        self.velocity = velocity
 
     """
     Find the distance between the cars current location and another point
@@ -60,14 +63,8 @@ class PurePursuit:
         self.look_forward_gain = look_forward_gain
         self.look_ahead_distance = look_ahead_distance
         self.path = path
-        #Needs to be refactored to look at our points instead of his cx and cy
         self.old_nearest_point_index = None
         self.old_steer_target = 0
-        self.cx = []
-        self.cy = []
-        for point in path:
-            self.cx.append(point.coordinate[0])
-            self.cy.append(point.coordinate[1])
 
     """
     Returns the steering angle in radians and the index to the goal points its turning to
@@ -75,24 +72,16 @@ class PurePursuit:
     """
     def pure_pursuit_steer_control(self, state: State) -> Tuple[float, int]:
         
-        # Getting the next point
         ind, Lf = self.search_target_index(state)
 
         # Checks to see if the previous found goal points is actually ahead of what it sees now
-        # If the previous index is ahead it uses that over the new one.
         if self.old_steer_target >= ind:
             ind = self.old_steer_target
 
-        if ind < len(self.cx):
-            tx = self.cx[ind]
-            ty = self.cy[ind]
-        else:  # toward goal
-            tx = self.cx[-1]
-            ty = self.cy[-1]
-            ind = len(self.cx) - 1
-
-        alpha = math.atan2(ty - state.rear_y, tx - state.rear_x) - state.yaw
-        delta = math.atan2(2.0 * state.wheel_base * math.sin(alpha) / Lf, 1.0)
+        #Steering angle of the front wheel
+        alpha = math.atan2(self.path[ind].coordinate[1] - state.rear_y, self.path[ind].coordinate[0] - state.rear_x) - state.yaw
+        #Getting the ackerman steering angle, the 1 is to ensure the angle is the correct sign
+        delta = math.atan2(2.0 * state.wheel_base * math.sin(alpha) / Lf, 1)
         self.old_steer_target = ind
 
         return delta, ind
@@ -107,8 +96,8 @@ class PurePursuit:
         if self.old_nearest_point_index is None:
             # search nearest point index
             # Mapping though all points in the path, and finding the difference in height and width to that point.
-            dx = [state.rear_x - icx for icx in self.cx]
-            dy = [state.rear_y - icy for icy in self.cy]
+            dx = [state.rear_x - point.coordinate[0] for point in self.path]
+            dy = [state.rear_y - point.coordinate[1] for point in self.path]
             #Getting the minimum hypotenues, aka the nearest point
             #For this firt time set it to the closest point.
             d = np.hypot(dx, dy)
@@ -117,15 +106,15 @@ class PurePursuit:
         else:
             #Get distance between the nearest point we found last time and the cars current posotiion.
             ind = self.old_nearest_point_index
-            distance_this_index = state.calc_distance(self.cx[ind], self.cy[ind])
+            distance_this_index = state.calc_distance(self.path[ind].coordinate[0], self.path[ind].coordinate[1])
             while True:
                 #Get the distance from the cars current posotion to the next point after the nearest point
-                distance_next_index = state.calc_distance(self.cx[ind + 1], self.cy[ind + 1])
+                distance_next_index = state.calc_distance(self.path[ind + 1].coordinate[0], self.path[ind + 1].coordinate[1])
                 #If the closest point is still the nearest point we found earlier exit the loop
                 if distance_this_index < distance_next_index:
                     break
                 # If the next next point is actually closer go to that instead and reapeat the calculation
-                ind = ind + 1 if (ind + 1) < len(self.cx) else ind
+                ind = ind + 1 if (ind + 1) < len(self.path) else ind
                 distance_this_index = distance_next_index
             self.old_nearest_point_index = ind
 
@@ -134,14 +123,16 @@ class PurePursuit:
         Lf = self.look_forward_gain * state.velocity + self.look_ahead_distance
 
         # search look ahead target point index
-        while Lf > state.calc_distance(self.cx[ind], self.cy[ind]):
-            if (ind + 1) >= len(self.cx):
+        while Lf > state.calc_distance(self.path[ind].coordinate[0], self.path[ind].coordinate[1]):
+            if (ind + 1) >= len(self.path):
                 break  # not exceed goal
             ind += 1
 
         return ind, Lf
 
-# Return yaw in radians from a quaterion
+"""
+Return yaw in radians from a quaterion
+"""
 def euler_from_quaternion(quaterion: Tuple[float, float, float, float]) -> float:
         x, y, z, w = quaterion
 
