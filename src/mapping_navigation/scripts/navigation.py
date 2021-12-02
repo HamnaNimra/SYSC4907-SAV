0dd766623864c1ceb1d867c01fa3e34009f77394
AndrewHall797 <andrew.hall286@gmail.com>
2021-12-02 11:45:04 -0500

    Cleaned things up, reformatted with pycharm suggestions, adding some aditional logging to navigation

--- src/mapping_navigation/scripts/navigation.py
+++ src/mapping_navigation/scripts/navigation.py
@@ -2,15 +2,13 @@
 
 import rospy
 import os
-from std_msgs.msg import Float64, String
 from geometry_msgs.msg import PoseStamped
 from pure_pursuit import *
 from std_msgs.msg import Float64
-from simple_path import *
+from path_helpers import *
 
 
 class Navigation:
-
     """
     This does navigation
     """
@@ -20,40 +18,47 @@ class Navigation:
         self.path = path
         self.target_index = 0
         self.navigator = PurePursuit(look_ahead_distance, look_forward_gain, path)
-        self.state = State(wheel_base)
+        self.car_state = CarState(wheel_base)
 
     def listener(self):
         rospy.init_node('navigation', anonymous=True)
         rospy.Subscriber('airsimPose', PoseStamped, self.handle_gps_data)
-        rospy.Subscriber("sensor/speed", Float64, self.handle_speed_data)        
+        rospy.Subscriber("sensor/speed", Float64, self.handle_speed_data)
         rate = rospy.Rate(30)
         # Once to get initial starting index
-        self.target_index, _ = self.navigator.search_target_index(self.state)
+        self.target_index = self.navigator.search_target_index(self.car_state)[0]
 
         while not rospy.is_shutdown():
-            if len(self.path)-1 > self.target_index:
+            if len(self.path) - 1 > self.target_index:
                 steering_angle = self.get_steering_angle()
                 self.steering_pub.publish(steering_angle)
             else:
-                rospy.loginfo("Done route")
+                rospy.loginfo('Done route')
             rate.sleep()
 
-    def handle_gps_data(self, postition: PoseStamped):
-        curr_point = Point((postition.pose.position.x, postition.pose.position.y))
-        quaterion = (postition.pose.orientation.x, postition.pose.orientation.y,
-                     postition.pose.orientation.z, postition.pose.orientation.w)
-        self.state.update_pos(curr_point, quaterion)
+    def handle_gps_data(self, position: PoseStamped):
+        curr_point = Point((position.pose.position.x, position.pose.position.y))
+        quaternion = (position.pose.orientation.x, position.pose.orientation.y,
+                      position.pose.orientation.z, position.pose.orientation.w)
+        self.car_state.update_pos(curr_point, quaternion)
 
     def handle_speed_data(self, speed: Float64):
-        self.state.update_speed(speed.data)
+        self.car_state.update_speed(speed.data)
+
+    """
+    Returns a steering angle for the car in Airsim range of -1 to 1
+    """
 
     def get_steering_angle(self) -> float:
-        ackerman_angle_rad, self.target_index = self.navigator.pure_pursuit_steer_control(self.state)
-        ackerman_angle_def = math.degrees(ackerman_angle_rad) / 45
-        return ackerman_angle_def
+        ackerman_angle_rad, self.target_index = self.navigator.pure_pursuit_steer_control(self.car_state)
+        ackerman_angle_ratio = math.degrees(ackerman_angle_rad) / 45
+        rospy.loginfo(f"""Target point is {path[self.target_index].coordinate[0]}, {path[self.target_index].coordinate[1]},
+                      car position {self.car_state.point.coordinate[0]}, {self.car_state.point.coordinate[1]}""")
+        rospy.loginfo(f'Current steering angle in degrees {math.degrees(ackerman_angle_rad)}')
+        return ackerman_angle_ratio
+
 
 if __name__ == "__main__":
-    #Current route stub enter the absolute path to the file here
     path = read_points(os.path.abspath(os.path.dirname(__file__)) + "/coords.txt")
     navigation = Navigation(look_ahead_distance=4.0, look_forward_gain=0.1, path=path, wheel_base=2.2)
     navigation.listener()
--- src/mapping_navigation/scripts/path_helpers.py
+++ src/mapping_navigation/scripts/path_helpers.py
@@ -1,48 +1,48 @@
 #!/usr/bin/env python3
 
-#Temp file to read in coordinates from the car, get a new point every 2 meters.
+# Temp file to read in coordinates from the car, get a new point every 2 meters.
 import numpy as np
 import airsim
 from map_information import *
 from typing import List
 
-#Write points to a file, can set the total distance and the distance between the points
-def write_points(filename: str, dist: int, delta: int):
 
+# Write points to a file, can set the total distance and the distance between the points
+def write_points(filename: str, dist: int, delta: int):
     client = airsim.CarClient()
     client.confirmConnection()
 
     with open(filename, "w") as coord_file:
 
-        #Get the starting point
-        pos_init = self.client.getCarState().kinematics_estimated.position
+        # Get the starting point
+        pos_init = client.getCarState().kinematics_estimated.position
         x_init = np.float16(pos_init.x_val)
         y_init = np.float16(pos_init.y_val)
         last_point = np.array(x_init, y_init)
 
-        #run for dist number of meters
+        # Run for dist number of meters
         for i in range(dist):
             new_point = False
 
-            #poll unit a new point is recorded
-            while(not new_point):
+            # poll unit a new point is recorded
+            while not new_point:
                 pos_curr = client.getCarState().kinematics_estimated.position
                 x_curr = np.float16(pos_curr.x_val)
                 y_curr = np.float16(pos_curr.y_val)
                 curr_point = np.array(x_curr, y_curr)
 
-                #Euclidian distance
+                # Euclidean distance
                 dist = np.linalg.norm(curr_point - last_point)
 
-                #record point if distance is greater than set delta
-                if(dist >= delta):
+                # record point if distance is greater than set delta
+                if dist >= delta:
                     new_point = True
                     coord_file.write(f'{x_curr}:{y_curr}\n')
                     last_point = curr_point
 
-#Read in points to a file as a list
-def read_points(filename: str) -> List[Point]:
 
+# Read in points to a file as a list
+def read_points(filename: str) -> List[Point]:
     points_list = []
     with open(filename) as coord_file:
         str_points = coord_file.readlines()
