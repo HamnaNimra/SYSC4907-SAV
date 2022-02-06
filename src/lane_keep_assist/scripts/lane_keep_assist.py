98ae302d5c917e0f5f8a23b2f17916e529ac0fd7
AndrewHall797 <andrew.hall286@gmail.com>
2022-02-06 18:09:02 -0500

    convert to lane message

--- src/lane_keep_assist/scripts/lane_keep_assist.py
+++ src/lane_keep_assist/scripts/lane_keep_assist.py
@@ -5,6 +5,7 @@ import lane_turn
 from std_msgs.msg import Float64, String
 from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import PointCloud, Image
+from lane_keep_assist.msg import LaneStatus
 
 
 class LaneKeepAssist:
@@ -13,64 +14,35 @@ class LaneKeepAssist:
     """
 
     def __init__(self):
-        self.steering_pub = rospy.Publisher('steering', Float64, queue_size=10)
-        self.lanes = None
-        self.new_steering_input = 0
-        self.curr_steering_input = 0
-        self.max_angle_deviation = 0.1
-        self.initial_angle = False
+        self.steering_pub_test = rospy.Publisher('lane_info', LaneStatus, queue_size=10)
+        self.lane_state = 1
 
     def listener(self):
         rospy.init_node('LaneKeepAssist', anonymous=True)
         rospy.Subscriber("airsim/image_raw", Image, self.handle_camera_data)
-        # rospy.Subscriber("carData", String, self.handle_car_data)
-        # rospy.Subscriber("airsimPose", PoseStamped, self.handle_gps_data)
-        # rospy.Subscriber("lidar", PointCloud, self.handle_lidar_data)
-        # rospy.Subscriber("curve_data", String, self.handle_curve_data)
 
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
 
-            # Ensures that the angle cannot change too much each run.
-            if self.initial_angle:
-                angle_deviation = self.new_steering_input - self.curr_steering_input
-                if abs(angle_deviation) > self.max_angle_deviation:
-                    self.curr_steering_input = int(
-                        self.curr_steering_input + self.max_angle_deviation * (angle_deviation / abs(angle_deviation)))
-                    rospy.loginfo(f'too much deviation {angle_deviation}')
-                else:
-                    self.curr_steering_input = self.new_steering_input
+            status = LaneStatus()
+            status.lane_info = self.lane_state
 
-            rospy.loginfo(f'Steering angle {self.new_steering_input}')
-            rospy.loginfo(f'Lanes {self.lanes}')
-            self.steering_pub.publish(self.new_steering_input)
+            self.steering_pub_test.publish(status)
             rate.sleep()
 
     # Handles the camera data
     def handle_camera_data(self, image):
-        self.lanes, steering_input, error = lane_turn.process_image(image)
-        if steering_input:
-            self.new_steering_input = steering_input / 45
-            self.initial_angle = True
+        self.lane_state = lane_turn.process_image(image)
 
-        if error:
-            rospy.loginfo(error)
+        if self.lane_state == 1:
+            rospy.loginfo("Two Lanes")
+        elif self.lane_state == 2:
+            rospy.loginfo("One Lane")
+        elif self.lane_state == 3:
+            rospy.loginfo("One Lane")
+        elif self.lane_state == 4:
+            rospy.loginfo("No Lanes")
 
-    # Handles the GPS data
-    # def handle_gps_data(self, data):
-    #     print("Got gps data")
-
-    # Handles the car data
-    # def handle_car_data(self, data):
-    #     print("Got car data")
-
-    # Handles the lidar data
-    # def handle_lidar_data(self, data):
-    #     print("Got lidar data")
-
-    # Handles the curve data
-    # def handle_curve_data(self, data):
-    #     print("Got curve data")
 
 if __name__ == "__main__":
     lane_keep = LaneKeepAssist()
--- src/lane_keep_assist/scripts/lane_turn.py
+++ src/lane_keep_assist/scripts/lane_turn.py
@@ -10,10 +10,9 @@ from cv_bridge import CvBridge, CvBridgeError
 import lane_detect
 
 
-def process_image(img: np.ndarray) -> Tuple[List, float]:
+def process_image(img: np.ndarray) -> int:
     bridge = CvBridge()
     height = img.height
-    width = img.width
 
     if img:
         try:
@@ -22,33 +21,21 @@ def process_image(img: np.ndarray) -> Tuple[List, float]:
             lane_lines = lane_detect.lane_detect(cv_img)
 
             if lane_lines:
-
-                # Depending on the lane lines the angle is calculated differently
-                # For two lanes just take the offset from the two lanes, then take the angle to the mid point
                 if len(lane_lines) == 2:
-                    _, _, left_x2, _ = lane_lines[0][0]
-                    _, _, right_x2, _ = lane_lines[1][0]
-                    mid = int(width / 2)
-                    x_offset = (left_x2 + right_x2) / 2 - mid
-                    y_offset = int(height / 2)
-
-                    angle_to_mid_radian = math.atan(x_offset / y_offset)  # angle (in radian) to center vertical line
-                    steering_angle = int(angle_to_mid_radian * 180.0 / math.pi) # angle (in degrees) to center vertical line
-
-                    return lane_lines, steering_angle, None
-                # Just take the angle from the one detected lane to the midpoint
+                    return 1
                 elif len(lane_lines) == 1:
                     x1, _, x2, _ = lane_lines[0][0]
                     x_offset = x2 - x1
                     y_offset = int(height / 2)
+                    angle_to_mid_radian = math.atan(x_offset / y_offset)
+                    steering_angle = int(angle_to_mid_radian * 180.0 / math.pi)
 
-                    angle_to_mid_radian = math.atan(x_offset / y_offset)  # angle (in radian) to center vertical line
-                    steering_angle = int(angle_to_mid_radian * 180.0 / math.pi) # angle (in degrees) to center vertical line
-
-                    return lane_lines, steering_angle, None
-            else:  # An error
-                print('Unable to detect lines...')
-                return None, None, None
+                    if steering_angle < 0:
+                        return 2
+                    if steering_angle > 0:
+                        return 3
+            else:
+                return 4
 
         except CvBridgeError as e:
-            return None, None, e
+            return 4
